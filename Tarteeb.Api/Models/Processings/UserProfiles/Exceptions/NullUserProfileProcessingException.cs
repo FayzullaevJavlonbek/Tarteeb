94a64cc29043522e13e52860f193dc7036fe5c3d
Max <sultonbek.rakhimov@gmail.com>
2023-07-11 11:51:48 +0500

    ShouldThrowValidationExceptionOnModifyIfUserProfileIsNullAndLogItAsync -> FAIL

---
+++ Tarteeb.Api/Models/Processings/UserProfiles/Exceptions/NullUserProfileProcessingException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace Tarteeb.Api.Models.Processings.UserProfiles.Exceptions
+{
+    public class NullUserProfileProcessingException : Xeption
+    {
+        public NullUserProfileProcessingException()
+            : base("User Profile is null.")
+        {  }
+    }
+}
